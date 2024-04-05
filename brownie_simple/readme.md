b27fc3acae710b35abb99c635df1b0b7f502b60f
tas-indranil <codearchitectdeveloper@gmail.com>
2024-04-05 10:08:46 +0530

    written test cases using brownie

--- brownie_simple/readme.md
+++ brownie_simple/readme.md
@@ -4,3 +4,5 @@
 3. Add accounts natively `brownie accounts new testAcc`
 4. Account list `brownie accounts list`
 5. Delete account `brownie accounts delete testAcc`
+6. Run Deploy script `brownie run scripts\deploy.py`
+7. For test files add `test` at the beginning
--- brownie_simple/scripts/deploy.py
+++ brownie_simple/scripts/deploy.py
@@ -1,9 +1,15 @@
-from brownie import accounts
+from brownie import accounts, config, SimpleStorage
 
 
 def deploy_simple_storage():
     account = accounts[0]
-    print(account)
+    # deploy contract
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    print(simple_storage.retrieve())
 
 
 def main():
