f3539f5dbf0cb26282715d9693ce20bafe4d5d69
tas-indranil <codearchitectdeveloper@gmail.com>
2024-04-08 11:46:19 +0530

    Created tests, added to live network

--- brownie_simple/readme.md
+++ brownie_simple/readme.md
@@ -6,3 +6,9 @@
 5. Delete account `brownie accounts delete testAcc`
 6. Run Deploy script `brownie run scripts\deploy.py`
 7. For test files add `test` at the beginning
+8. OK! Don't do this in a production environment. Please Don't: `brownie test -W ignore` This is to ignore the warnings from python 3.12
+9. Get all the network list `brownie networks list`
+10. If you want to test just one function `brownie test -k  function_name`
+11. Use `brownie test -s -W ignore` to get moe information. All of this is from PyTest so just go through docs
+12. Deploy to testnet, just add the project key to `.env` file, and run `brownie run scripts\deploy.py --network rinkeby` [rinkeby for me]
+13. To use brownie console use `brownie console`
--- brownie_simple/tests/test_simple_storage.py
+++ brownie_simple/tests/test_simple_storage.py
@@ -1,12 +1,29 @@
-from brownie import accounts, SimpleStorage
+from brownie import accounts, SimpleStorage, network, config
 
 
 def test_deploy():
     # Arrange
-    account = accounts[0]
+    # account = accounts[0]
+    account = get_account()
     # Act
     simple_storage = SimpleStorage.deploy({"from": account})
     starting_value = simple_storage.retrieve()
     expected = 0
     # Assert
     assert starting_value == expected
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0];
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def test_updating_storage():
+    account = accounts[0]  # Arrange
+    simple_storage = SimpleStorage.deploy({"from": account})  # Arrange
+    transaction = simple_storage.store(15, {"from": account})  # Act
+    transaction.wait(1)
+    starting_value = simple_storage.retrieve()
+    assert simple_storage.retrieve() == 15  # Assert
